bdea67d2f50d8e9bf2c04de375507daf6260444a
mo saleh <msalehy77@gmail.com>
2021-09-10 09:43:20 +0430

    fix

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const fs = require("fs")
-const lighthouse = require("lighthouse")
-const chromeLauncher = require("chrome-launcher")
-const url = require('url')
+const fs = require("fs");
+const lighthouse = require("lighthouse");
+const chromeLauncher = require("chrome-launcher");
+const url = require("url");
 
 const adrs = fs.readFileSync("Adrs.csv").toString().split("\n");
-const strategys = ['mobile', 'desktop'];
+const strategys = ["mobile", "desktop"];
 
 (async () => {
-  const chrome = await chromeLauncher.launch({ chromeFlags: ["--headless"] })
+  const chrome = await chromeLauncher.launch({ chromeFlags: ["--headless"] });
   const options = {
     // logLevel: "info",
     output: "html",
     port: chrome.port,
-  }
+  };
 
   for (adr of adrs) {
     const parsedAdr = url.parse(adr, true);
@@ -20,14 +20,12 @@ const strategys = ['mobile', 'desktop'];
       options.strategy = strategy;
       const runnerResult = await lighthouse(adr, options);
       const reportHtml = runnerResult.report;
-      fs.writeFileSync(`lh-${parsedAdr.host}-${parsedAdr.pathname.split('/').join('I')}-${strategy}.html`, reportHtml);
+      fs.writeFileSync(
+        `lh-${parsedAdr.pathname.split("/").join("I")}-${strategy}.html`,
+        reportHtml
+      );
     }
   }
 
-  await chrome.kill()
-})()
-
-
-
-
-
+  await chrome.kill();
+})();
